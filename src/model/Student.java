e2313524fbb5c0a66db511d46169a92d954908be
LaoBing_Fung <FLB_B401@163.com>
2019-02-13 20:54:46 +0800

    add constructor of class Student

---
+++ src/model/Student.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Student {
+    private String name;
+    private int age;
+    private String gender;
+    private String studentID;
+
+    public Student(String name, int age, String gender, String studentID) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.studentID = studentID;
+    }
+}
